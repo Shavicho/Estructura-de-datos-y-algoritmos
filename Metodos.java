3cb07ebc6e7b4e4a3e7f83b5ab422b41f7ad19a0
Javier Coronado <jcoronado@unsa.edu.pe>
2023-05-07 18:24:22 -0500

    metodos

---
+++ Metodos.java
@@ -0,0 +1,10 @@
+public class Metodos {
+    public static void main(String[] args) {
+        miMetodo();
+    }
+    
+    static void miMetodo()
+    {
+        System.out.println("llamando este metodo");
+    }
+}
