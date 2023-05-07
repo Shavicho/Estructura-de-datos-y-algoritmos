46655279303d7f812a9849c6947547b19fc799f0
Javier Coronado <jcoronado@unsa.edu.pe>
2023-05-07 17:56:08 -0500

    variables

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables 
+{
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println(name);
+        int myNum = 15;
+        System.out.println(myNum);
+    }
+            
+    
+}
