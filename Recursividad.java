f21637f9027ac48e3028b190b23ad3cc0c03d9e6
Javier Coronado <jcoronado@unsa.edu.pe>
2023-05-07 18:22:24 -0500

    recursividad

---
+++ Recursividad.java
@@ -0,0 +1,13 @@
+public class Recursividad {
+    public static void main(String[] args) {
+        int resultado=suma(10);
+        System.out.println(resultado);
+    }
+    public static int suma(int k) {
+    if (k > 0) {
+      return k + suma(k - 1);
+    } else {
+      return 0;
+    }
+  }
+}
