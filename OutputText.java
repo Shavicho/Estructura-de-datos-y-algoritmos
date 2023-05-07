0ffa77e899f2f53246e0ad2193723487ede8b2bb
Javier Coronado <jcoronado@unsa.edu.pe>
2023-05-07 17:40:15 -0500

    texto

---
+++ OutputText.java
@@ -0,0 +1,9 @@
+public class OutputText 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+  }
+    
+}
