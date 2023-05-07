9d2ee9c32f9a5c617cd19fc625b321026c1272cf
Javier Coronado <jcoronado@unsa.edu.pe>
2023-05-07 17:58:32 -0500

    operadores

---
+++ Operators.java
@@ -0,0 +1,16 @@
+public class Operators {
+    public static void main(String[] args) {
+        
+        int x = 10;
+        x += 3;
+        System.out.println(x);
+        x -= 3;
+        System.out.println(x);
+        x *= 3;
+        System.out.println(x);
+        x /= 3;
+        System.out.println(x);
+                
+        
+    }
+}
