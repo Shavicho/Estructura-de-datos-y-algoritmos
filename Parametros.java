f87f97e1b1f5f423854500b7fbe71fa43af06834
Javier Coronado <jcoronado@unsa.edu.pe>
2023-05-07 18:23:37 -0500

    parametros

---
+++ Parametros.java
@@ -0,0 +1,11 @@
+public class Parametros {
+    public static void main(String[] args) {
+        Nombres("Javier");
+        Nombres("Pamela");
+        Nombres("Gonzalo");
+    }
+    static void Nombres(String nombre)
+    {
+        System.out.println("el nombre es "+nombre);
+    }
+}
